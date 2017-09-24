8fb998355ad88c587343f54760b48c178d60a908
Fernando Huber <fernando.huber@gmail.com>
2017-09-23 20:19:18 -0700

    test2

--- wordpress/wp-content/themes/portfolio/header.php
+++ wordpress/wp-content/themes/portfolio/header.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
     <title><?php bloginfo('name'); ?></title>
     <?php wp_head(); ?>
   </head>
