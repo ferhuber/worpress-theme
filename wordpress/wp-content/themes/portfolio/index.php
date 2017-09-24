abd0b489672bf49395e80039326ea982aa88114d
Fernando Huber <fernando.huber@gmail.com>
2017-09-23 21:37:01 -0700

    update container widt 100%

--- wordpress/wp-content/themes/portfolio/index.php
+++ wordpress/wp-content/themes/portfolio/index.php
@@ -9,7 +9,6 @@ if (have_posts()) :
 <div class="main-page">
 
   <article class="post">
-    <h1> test!!!678 </h1>
 
     <?php the_post_thumbnail(); ?>
     <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
