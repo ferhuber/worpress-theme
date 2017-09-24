b38f77ea7ad5623a980ada5a84f145743cabe0c4
Fernando Huber <fernando.huber@gmail.com>
2017-09-23 21:19:14 -0700

    add logo

--- wordpress/wp-content/themes/portfolio/page-about-me.php
+++ wordpress/wp-content/themes/portfolio/page-about-me.php
@@ -6,10 +6,14 @@ get_header();
 if (have_posts()) :
   while (have_posts()) : the_post(); ?>
 
+  <div class="main-page">
+
+
   <article class="post">
     <h2><?php the_title(); ?></h2>
     <?php the_content(); ?>
   </article>
+</div>
 
   <?php endwhile;
 
@@ -17,8 +21,6 @@ if (have_posts()) :
     echo '<p>No content found</p>';
 
  endif;
-
 get_footer();
 
 ?>
-
