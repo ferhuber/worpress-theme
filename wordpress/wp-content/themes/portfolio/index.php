d3d4d7e6a2003f301f101b4c898e0e91ed6e0003
Fernando Huber <fernando.huber@gmail.com>
2017-09-23 19:35:55 -0700

    test

--- wordpress/wp-content/themes/portfolio/index.php
+++ wordpress/wp-content/themes/portfolio/index.php
@@ -7,7 +7,7 @@ if (have_posts()) :
   while (have_posts()) : the_post(); ?>
 
   <article class="post">
-<h1> test </h1>
+<h1> test!!! </h1>
 
     <?php the_post_thumbnail(); ?>
     <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
@@ -18,7 +18,7 @@ if (have_posts()) :
 
   else :
     echo '<p>No content found</p>';
- 
+
  endif;
 
 get_footer();
