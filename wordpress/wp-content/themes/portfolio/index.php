e0e7e9ece46d69dc645bf66a0bef47bc752dd6b5
Fernando Huber <fernando.huber@gmail.com>
2017-09-23 19:37:44 -0700

    test2

--- wordpress/wp-content/themes/portfolio/index.php
+++ wordpress/wp-content/themes/portfolio/index.php
@@ -7,7 +7,7 @@ if (have_posts()) :
   while (have_posts()) : the_post(); ?>
 
   <article class="post">
-<h1> test!!! </h1>
+<h1> test!!!1234 </h1>
 
     <?php the_post_thumbnail(); ?>
     <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
