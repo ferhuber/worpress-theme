423a15b7f3eec4671b032ed6a9fce86e5076e9e7
Fernando Huber <fernando.huber@gmail.com>
2017-09-23 20:15:49 -0700

    test2

--- wordpress/wp-content/themes/portfolio/header.php
+++ wordpress/wp-content/themes/portfolio/header.php
@@ -11,20 +11,17 @@
 
  <div class="container">
   <header class="site-header">
-    <h1><?php bloginfo('name'); ?></h1>
-    <h5><?php bloginfo('description'); ?></h5>
-    
+    <!-- <h1><?php bloginfo('name'); ?></h1>
+    <h5><?php bloginfo('description'); ?></h5> -->
+
     <nav class="site-nav">
       <?php
-      
+
       $args = array(
-        'theme_location' => 'primary' 
+        'theme_location' => 'primary'
       );
 
-      ?> 
+      ?>
       <?php wp_nav_menu( $args); ?>
     </nav>
  </header>
-  
-
-
